c1e2368b70fec44fb9d18a855c0ea389255e944c
Gabriel Barbosa Pereira <superxl8gbpaa@outlook.com>
2021-02-25 17:49:05 -0300

    Some chances
    Starting the project but still nothing important

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
+std :: string line(int v = 50, char c = '-');
+
 int main(){
-    std :: cout << "Hello World!\n";
-    system("pause > null");
+    int c{0};
+    std :: cout << line(38) << "\n";
+    std :: cout << "Welcome to the best bank in the world!\nAre you an adm or a client?\n";
+    std :: cout << "Choose 0 for adm or choose 1 for user: ";
+    std :: cin >> c;
+    system("pause");
     return 0;
+}
+
+std :: string line(int v, char c){
+    std :: string r;
+    for(int x{0}; x < v; x++){
+        r += c;
+    }
+    return r;
 }
